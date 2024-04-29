c3b04f19897ba0f8090e55e30eb3d7f731a81990
Kuba <[inkub9999@gmail.com]>
2024-04-29 13:35:52 +0200

    ch

--- app.py
+++ app.py
@@ -303,11 +303,11 @@ if choose__phrase != st.session_state.previous_choose_phrase or miasto != st.ses
                             st.write(f'🤖 Niestety, nie znaleziono miejsc spełniających Twoje oczekiwania.')
                 except:
                     try:
-                    st.session_state.choose_rec = df_rec.index[df_rec['title'] == query_results[3].payload["title"]].tolist()[0]
-                    if zgodnosc(df_rec, st.session_state.choose_rec, choose__phrase_tr) != '1':
-                        st.session_state.choose_rec = 0
-                        with st.sidebar:
-                            st.write(f'🤖 Niestety, nie znaleziono miejsc spełniających Twoje oczekiwania.')
+                        st.session_state.choose_rec = df_rec.index[df_rec['title'] == query_results[3].payload["title"]].tolist()[0]
+                        if zgodnosc(df_rec, st.session_state.choose_rec, choose__phrase_tr) != '1':
+                            st.session_state.choose_rec = 0
+                            with st.sidebar:
+                                st.write(f'🤖 Niestety, nie znaleziono miejsc spełniających Twoje oczekiwania.')
                     except:
                         try:
                             st.session_state.choose_rec = df_rec.index[df_rec['title'] == query_results[4].payload["title"]].tolist()[0]
